6cfb1b85bfc15bcc72753897c38bd5258822171e
NCSaketh <nc.saketh1@gmail.com>
2020-10-16 01:07:35 +0530

    TC1.3_Pos3: max at pos 3

--- src/test/java/com/cg/findmax/FindMaxTest.java
+++ src/test/java/com/cg/findmax/FindMaxTest.java
@@ -2,17 +2,24 @@ package com.cg.findmax;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 public class FindMaxTest {
-        @Test
-        public void givenMaxAtPos1_ReturnTrue() {
-            int result = FindMax.FindMaxInt(3,2,1);
-            Assert.assertEquals(3, result);
-        }
+    @Test
+    public void givenMaxAtPos1_ReturnTrue() {
+        int result = FindMax.FindMaxInt(3, 2, 1);
+        Assert.assertEquals(3, result);
+    }
 
     @Test
     public void givenMaxAtPos2_ReturnTrue() {
-        int result = FindMax.FindMaxInt(2,3,1);
+        int result = FindMax.FindMaxInt(2, 3, 1);
         Assert.assertEquals(3, result);
     }
+
+    @Test
+    public void givenMaxAtPos3_ReturnTrue() {
+        int result = FindMax.FindMaxInt(2, 1, 3);
+        Assert.assertEquals(3, result);
     }
+}
 
