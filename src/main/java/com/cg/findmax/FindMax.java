0645f1ef9d4baa064cd582e53087de3559ecdc39
NCSaketh <nc.saketh1@gmail.com>
2020-10-16 00:49:47 +0530

    UC1 : Find int max

--- src/main/java/com/cg/findmax/FindMax.java
+++ src/main/java/com/cg/findmax/FindMax.java
@@ -2,6 +2,20 @@ package com.cg.findmax;
 
 public class FindMax {
 
+    public static void main(String[] args) {
+        FindMax m=new FindMax();
+    }
+
+    public Integer FindMax(Integer a,Integer b,Integer c)
+    {
+        if (a >= b && a >= c)
+            return a;
+        else if (b >= a && b >= c)
+            return b;
+        else
+            return c;
+    }
+
 
 }
 
