2c51514493002d3893652b4781491d8bbed94104
NCSaketh <nc.saketh1@gmail.com>
2020-10-16 01:05:37 +0530

    TC1.2 : max at pos 2

--- src/test/java/com/cg/findmax/FindMaxTest.java
+++ src/test/java/com/cg/findmax/FindMaxTest.java
@@ -8,5 +8,11 @@ public class FindMaxTest {
             int result = FindMax.FindMaxInt(3,2,1);
             Assert.assertEquals(3, result);
         }
+
+    @Test
+    public void givenMaxAtPos2_ReturnTrue() {
+        int result = FindMax.FindMaxInt(2,3,1);
+        Assert.assertEquals(3, result);
+    }
     }
 
